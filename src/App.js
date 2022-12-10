ea26d4c0a810ce42a9061828c1b8d9f2e37a90d2
kartikeya pandey <pandeykartikeya0604@gmail.com>
2022-12-10 14:02:36 +0530

    gtag add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { Route, useLocation } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>ga test app</h1>
-    </div>
-  );
+    const location = useLocation();
+
+    useEffect( () => {
+        window.gtag( 'event', 'page_view', {
+            page_title: 'home',
+            page_path: location.pathname + location.search,
+            page_location: window.location.href
+        } )
+    }, [location] );
+
+    return (
+        <div className="App">
+            <h1>ga test app</h1>
+        </div>
+    );
 }
 
 export default App;
